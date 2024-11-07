c8f1936331959e0c1e35ec330bd6037fdeaf9cb6
frankafan <frank.a.fan@outlook.com>
2024-11-07 14:24:41 -0500

    Save plots.

--- analysis.py
+++ analysis.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_cuts = 6
-num_requests = 6
+num_cuts = 40
+num_requests = 100
 
 file_path = f'loss_{num_cuts}_{num_requests}.npy'
 losses = np.load(file_path, allow_pickle=True)
 
 
+plt.figure()
 for loss in losses:
     plt.plot(loss)
 
@@ -15,4 +16,5 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.title(f'Minimum Loss: {np.min(np.concatenate(losses))}')
 plt.legend([f'Min Loss: {np.min(losses[i]):.2f}' for i in range(len(losses))])
+plt.savefig(f'loss_{num_cuts}_{num_requests}.png')
 plt.show()
